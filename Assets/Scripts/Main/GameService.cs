3602cf299bde6d88142a70e29afaddcfc08d99e3
Dip Patel <pateldeep2332@gmail.com>
2025-04-17 00:55:06 +0530

    Used Service Locator for remaining services

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,5 +1,10 @@
+using ServiceLocator.Events;
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +13,38 @@ public class GameService : GenericMonoSingleton<GameService>
 { 
     public PlayerService playerService {  get; private set; }
 
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    public SoundService soundService { get; private set; }
+
+    public WaveService waveService { get; private set; }
+
+    public MapService mapService { get; private set; }
+
+    public EventService eventService { get; private set; }
+
+    [SerializeField] private UIService uiService;
+
+    public UIService UIService => uiService;
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+
+
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
+
+
 
     private void Start()
     {
+        eventService = new EventService();
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        waveService = new WaveService(waveScriptableObject);
+        mapService = new MapService(mapScriptableObject);
     }
 
     private void Update()
